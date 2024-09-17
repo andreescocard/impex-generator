04b0955991d76a46e6ac82616b61571a0d15fec9
andreescocard <andre.escocard@hesea.com.br>
2024-09-16 23:35:11 -0300

    fix: document error

--- org/impexgenerator/src/app/pages/components/generator.component.ts
+++ org/impexgenerator/src/app/pages/components/generator.component.ts
@@ -1,46 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-generator',
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './generator.component.html',
   styleUrls: ['./generator.component.css'],
   standalone: true,
 })
 export class GeneratorComponent implements OnInit {
-    
- 
-  options = ['Choose one','Paragraph', 'Feature Flag', 'Page', 'Product Images'];
+  options = ['Choose one','Feature Flag', 'Paragraph',  'Page', 'Product Images'];
   selectedOption: string = 'Choose one';
   darkMode = true;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   ngOnInit(): void {
-    this.applyDarkMode(this.darkMode);
+    if (isPlatformBrowser(this.platformId)) {
+      this.applyDarkMode(this.darkMode);
+    }
   }
 
   toggleDarkMode() {
-    this.darkMode = !this.darkMode;
-    if (this.darkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
+    if (isPlatformBrowser(this.platformId)) {
+      this.darkMode = !this.darkMode;
+      if (this.darkMode) {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
     }
   }
 
   submit() {
-    // Handle submit logic here
     console.log('Submit button clicked with option:', this.selectedOption);
   }
 
   private applyDarkMode(isDark: boolean) {
-    const root = document.documentElement;
-    if (isDark) {
-      root.classList.add('dark');
-    } else {
-      root.classList.remove('dark');
+    if (isPlatformBrowser(this.platformId)) {
+      const root = document.documentElement;
+      if (isDark) {
+        root.classList.add('dark');
+      } else {
+        root.classList.remove('dark');
+      }
     }
   }
-
 }
